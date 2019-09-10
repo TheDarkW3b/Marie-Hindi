97f10523246af1d1c6f59570800e2f3bd0eef0f4
DarkWeb <54339318+TheDarkW3b@users.noreply.github.com>
2019-09-10 11:37:04 +0530

    Update global_bans.py

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -91,7 +91,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
 
         return
 
-    message.reply_text("⚡️ *Snaps the Banhammer* ⚡️")
+    message.reply_text("⚡️ *Another Bitch Goes Off* ⚡️")
 
     banner = update.effective_user  # type: Optional[User]
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
