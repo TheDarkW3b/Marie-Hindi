8a14083b716ad33a5a19c35a1a757032796ac345
DarkWeb <54339318+TheDarkW3b@users.noreply.github.com>
2019-09-10 09:05:44 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,25 +18,16 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+Hoi {}, my name is {}! I'm here to help you manage your groups!. Need Any Help Join Our Support Group ( @MenheraChanSupport) 
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
+I'm a group manager bot maintained by this[sexy boi](tg://user?id={}) I'm built in python3, using the python-telegram-bot library.
 
-My future updates will be put into This Channel - @MarieChechi, Support Group @InFoTelGroup and 
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
-you can create clone same like me..
+You can find the list of available commands with /help.
 
-Using standard [Marie](http://telegram.dog/BanhammerMarie_bot) is always gonna be easier.
-Perks:
-- latest features
-- it's free
-- biggest list of users for any /info commands
-- the database will always be correctly formatted
-- bugs will always be fixed ASAP
-- no need to maintain anything yourself
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
+If you're enjoying using me, and or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
 
-"""
+[Click here to add me in your groups](t.me/MenheraChan_Bot?startgroup=true)"
+""" 
 
 HELP_STRINGS = """
 
@@ -55,10 +46,10 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
+It took lots of work for [my creator](t.me/TheDarkW3b) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+If You Really Wanna Donate Pay to My [Master](t.me/TheDarkW3b) """
 
 IMPORTED = {}
 MIGRATEABLE = []
