acdc8db20d49c8d10eeda8fad3281996fe98ca10
DarkWeb <54339318+TheDarkW3b@users.noreply.github.com>
2019-10-19 07:25:18 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,7 +20,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 नमस्ते {}, मेरा नाम {} है! मैं यहां आपके ग्रूप को मैनज करने में आपकी सहायता करने के लिए हूं!.अगर आपके पास मुझे इस्तेमाल करने का कोई प्रश्न है तो /help पर क्लिक करे
 
-मैं ग्रुप मैनेजर बॉट हूं,मेरे [भगवान](tg://user?id={}) ये है,मेरे भविष्य के अपडेट इस चैनल में डाले जाएंगे - @MarieChechi & और मेरा सपोर्ट ग्रुप @InFoTelGroup
+मैं ग्रुप मैनेजर बॉट हूं,मेरे [भगवान](tg://user?id={}) ये है,मेरे भविष्य के अपडेट इस चैनल में डाले जाएंगे - @MarieChechi & और मेरा सपोर्ट ग्रुप @CuratorCrew है!
 
 यह मेरा [डिप्लॉय कोड है](https://heroku.com/deploy?template=https://github.com/TheDarkW3b/Marie-Hindi), आप मेरी तरह ही क्लोन बना सकते हैं।
 
